1d0411b671855abb1dad2ffa18604e1417ebcd9e
Gajus Kuizinas <gajus@gajus.com>
2018-07-09 16:17:47 +0100

    feat: add match subroutine

---
+++ src/subroutines/matchSubroutine.js
@@ -0,0 +1,38 @@
+// @flow
+
+import parseRegex from 'regex-parser';
+import {
+  vsprintf
+} from 'sprintf-js';
+import {
+  SurgeonError
+} from '../errors';
+import {
+  InvalidValueSentinel
+} from '../sentinels';
+
+const matchSubroutine = (subject: string, [userRule, sprintfFormat]: $ReadOnlyArray<string>) => {
+  const rule: RegExp = parseRegex(userRule);
+
+  if (typeof subject !== 'string') {
+    throw new SurgeonError('Input is not a string.');
+  }
+
+  const matches = subject.match(rule);
+
+  if (!matches) {
+    return new InvalidValueSentinel('input does not match "' + rule.toString() + '" regular expression');
+  }
+
+  if (matches.length === 1) {
+    throw new SurgeonError('Regular expression must define at least one capturing group.');
+  }
+
+  if (matches.length > 2 && !sprintfFormat) {
+    throw new SurgeonError('Must define sprintf template when matching multiple groups.');
+  }
+
+  return vsprintf(sprintfFormat || '%s', matches.slice(1));
+};
+
+export default matchSubroutine;
---
+++ test/surgeon/subroutines/matchSubroutine.js
@@ -0,0 +1,37 @@
+// @flow
+
+import test from 'ava';
+import {
+  InvalidValueSentinel
+} from '../../../src';
+import matchSubroutine from '../../../src/subroutines/matchSubroutine';
+
+test('throws an error if invoked with invalid RegExp', (t): void => {
+  t.throws((): void => {
+    matchSubroutine('foo', ['/foo/x']);
+  });
+});
+
+test('throws an error if regex does not define capturing groups', (t) => {
+  t.throws((): void => {
+    matchSubroutine('foo', ['foo']);
+  }, 'Regular expression must define at least one capturing group.');
+});
+
+test('returns InvalidValueSentinel when input cannot be matched', (t) => {
+  t.true(matchSubroutine('baz', ['(foo)']) instanceof InvalidValueSentinel);
+});
+
+test('matches a single capturing group', (t) => {
+  t.true(matchSubroutine('foobar', ['(foo)']) === 'foo');
+});
+
+test('throws an error when matching multiple capturing groups without sprintf template', (t) => {
+  t.throws((): void => {
+    matchSubroutine('foobar', ['(foo)(bar)']);
+  }, 'Must define sprintf template when matching multiple groups.');
+});
+
+test('formats multiple capturing group matches using sprintf', (t) => {
+  t.true(matchSubroutine('foobar', ['(foo)(bar)', '%2$s-%1$s']) === 'bar-foo');
+});
