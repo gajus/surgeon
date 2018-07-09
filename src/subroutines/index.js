74c5fcfd3d09a9c61646be17992af04172300f3f
Gajus Kuizinas <gajus@gajus.com>
2018-07-09 16:24:22 +0100

    feat: add match subroutine

--- src/subroutines/index.js
+++ src/subroutines/index.js
@@ -1,5 +1,6 @@
 // @flow
 
+export {default as matchSubroutine} from './matchSubroutine';
 export {default as nextUntilSubroutine} from './nextUntilSubroutine';
 export {default as readSubroutine} from './readSubroutine';
 export {default as removeSubroutine} from './removeSubroutine';
