c9d460613bb35457b46e141b584123f4f706ea25
Christian Westgaard <cwe@enonic.com>
2018-10-25 14:16:32 +0200

    Use function overloading to allow optional first argument selector to modifySubroutine

--- src/subroutines/modifySubroutine.js
+++ src/subroutines/modifySubroutine.js
@@ -6,19 +6,62 @@ import {
 import type {
   ModifySubroutineType
 } from '../types';
+import Logger from '../Logger';
 
-const modifySubroutine: ModifySubroutineType = (subject, [target, name, value], {evaluator}) => {
+const log = Logger.child({
+  namespace: 'subroutine:select'
+});
+
+const modifySubroutine: ModifySubroutineType = (subject, [selector, target, name, value], {evaluator}) => {
+  log.debug('arguments "%s"', JSON.stringify({
+    selector,
+    target,
+    name, // eslint-disable-line sort-keys
+    value
+  }, null, 4));
+
   if (!evaluator.isElement(subject)) {
     throw new SurgeonError('Unexpected value. Value must be an element.');
   }
 
-  if (target === 'attribute') {
-    return evaluator.setAttributeValue(subject, name, value);
-  } else if (target === 'property') {
-    return evaluator.setPropertyValue(subject, name, value);
-  } else {
+  // Function overloading
+  if (typeof value === 'undefined') {
+    /* eslint-disable no-param-reassign*/
+    value = name;
+    name = target;
+    target = selector;
+    selector = undefined;
+    /* eslint-enable no-param-reassign*/
+  }
+
+  log.debug('overloaded arguments "%s"', JSON.stringify({
+    selector,
+    target,
+    name, // eslint-disable-line sort-keys
+    value
+  }, null, 4));
+
+  if (!(target === 'attribute' || target === 'property')) {
     throw new SurgeonError('Unexpected modify target.');
   }
+
+  if (selector) {
+    evaluator.querySelectorAll(subject, selector).forEach((match) => {
+      if (target === 'attribute') {
+        return evaluator.setAttributeValue(match, name, value);
+      }
+
+      return evaluator.setPropertyValue(match, name, value);
+    });
+
+    return subject;
+  }
+
+  if (target === 'attribute') {
+    return evaluator.setAttributeValue(subject, name, value);
+  }
+
+  return evaluator.setPropertyValue(subject, name, value);
 };
 
 export default modifySubroutine;
--- test/surgeon/subroutines/modifySubroutine.js
+++ test/surgeon/subroutines/modifySubroutine.js
@@ -5,6 +5,7 @@ import sinon from 'sinon';
 import cheerioEvaluator from '../../../src/evaluators/cheerioEvaluator';
 import modifySubroutine from '../../../src/subroutines/modifySubroutine';
 import readSubroutine from '../../../src/subroutines/readSubroutine';
+import selectSubroutine from '../../../src/subroutines/selectSubroutine';
 
 test('uses evalutor.isElement to validate the subject', (t): void => {
   const isElement = sinon.stub().returns(false);
@@ -20,6 +21,24 @@ test('uses evalutor.isElement to validate the subject', (t): void => {
   t.true(error.message === 'Unexpected value. Value must be an element.');
 });
 
+test('modifying multiple attributes using selector works and returns the modified dom', (t): void => {
+  const evaluator = cheerioEvaluator();
+  const {parseDocument} = evaluator;
+  const dom = parseDocument(`<body>
+  <a href="/relative1">Relative link 1</div>
+  <a href="/relative2">Relative link 2</div>
+</body>`);
+  const value = 'newValue';
+  const modifiedDom = modifySubroutine(dom, ['body a', 'attribute', 'href', value], {evaluator});
+
+  // $FlowFixMe
+  selectSubroutine(modifiedDom, ['a', '{0,}'], {evaluator}).forEach((el) => { // eslint-disable-line no-extra-parens
+    const result = readSubroutine(el, ['attribute', 'href'], {evaluator});
+
+    t.true(result === value);
+  });
+});
+
 test('modifying an attribute works and returns modified node', (t): void => {
   const evaluator = cheerioEvaluator();
   const {parseDocument, querySelectorAll} = evaluator;
@@ -30,6 +49,24 @@ test('modifying an attribute works and returns modified node', (t): void => {
   const result = readSubroutine(modifiedNode, ['attribute', 'href'], {evaluator});
 
   t.true(result === value);
+});
+
+test('modifying multiple properties works and returns the modified dom', (t): void => {
+  const evaluator = cheerioEvaluator();
+  const {parseDocument} = evaluator;
+  const dom = parseDocument(`<body>
+  <input type="checkbox"/>
+  <input type="checkbox"/>
+</body>`);
+  const value = true;
+  const modifiedDom = modifySubroutine(dom, ['body input', 'property', 'checked', value], {evaluator});
+
+  // $FlowFixMe
+  selectSubroutine(modifiedDom, ['body input', '{0,}'], {evaluator}).forEach((el) => {
+    const result = readSubroutine(el, ['property', 'checked'], {evaluator});
+
+    t.true(result === value);
+  });
 });
 
 test('modifying an property works and returns modified node', (t): void => {
